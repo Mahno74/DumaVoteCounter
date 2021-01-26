eb7adafaa1377d51dcc74ad43b32aa1a732a3e13
Юрасов Вячеслав <2665603@gmail.com>
2021-01-26 08:39:03 +0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DumaVoteCounter {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Left = Properties.Settings.Default.positionX;
+            Top = Properties.Settings.Default.positionY;
+            Width = Properties.Settings.Default.windowWidth;
+            Height = Properties.Settings.Default.windowHeight;
+        }
+
+        private void Dragging(object sender, MouseButtonEventArgs e) {
+            this.DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            Properties.Settings.Default.positionX = Left;
+            Properties.Settings.Default.positionY = Top;
+            Properties.Settings.Default.windowWidth = Width;
+            Properties.Settings.Default.windowHeight = Height;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
